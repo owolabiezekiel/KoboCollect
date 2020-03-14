5a8958104139e3d7f0552cb43430b876e9b134aa
owolabiezekiel <owo.ezekiel@gmail.com>
2020-03-14 14:59:23 +0100

    first changes

--- collect_app/src/main/java/org/koboc/collect/android/activities/MainMenuActivity.java
+++ collect_app/src/main/java/org/koboc/collect/android/activities/MainMenuActivity.java
@@ -344,10 +344,10 @@ public class MainMenuActivity extends Activity {
     		menu.add(0, MENU_PREFERENCES, 0, R.string.general_preferences)
 				.setIcon(R.drawable.ic_menu_preferences),
 			MenuItem.SHOW_AS_ACTION_NEVER);
-		CompatibilityUtils.setShowAsAction(
+		/*CompatibilityUtils.setShowAsAction(
     		menu.add(0, MENU_ADMIN, 0, R.string.admin_preferences)
 				.setIcon(R.drawable.ic_menu_login),
-			MenuItem.SHOW_AS_ACTION_NEVER);
+			MenuItem.SHOW_AS_ACTION_NEVER);*/
 		return true;
 	}
 
